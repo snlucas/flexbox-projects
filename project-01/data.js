dc439f26aeb28a3644eac6c4649e2385ee74b318
snlucas <lucas.lambda.101@gmail.com>
2026-03-11 13:59:56 -0300

    Add data constant

---
+++ project-01/data.js
@@ -0,0 +1,9 @@
+export const DATA_BAR = [
+    { height: 120, color: '#9b9aff' },
+    { height: 140, color: '#ffb471' },
+    { height: 125, color: '#ff98d1' },
+    { height: 160, color: '#98fb98' },
+    { height: 190, color: '#9dd1ff' },
+    { height: 320, color: '#ff9b9b' },
+    { height: 140, color: '#ffff99' }
+];
